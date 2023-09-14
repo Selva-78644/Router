a3c49ff028116a5c53074eea607b264445e7f274
Selva Kumar <133832679+Selva-78644@users.noreply.github.com>
2023-09-14 19:54:29 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import { Routes, Route, Form } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
+function App() {
+  return (
+    <div className="App"> 
+        <Form/>    
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path="from" element={<Form/>}/>
+        </Route>
+      </Routes>
+    </div>
+  );
+ }
+
+ export default App;
